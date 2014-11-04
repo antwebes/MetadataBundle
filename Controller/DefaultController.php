b7a33bcd246307cf008a8fad780b64fa625ffc9f
Chrysweel <pablitoen05@hotmail.com>
2014-11-04 17:41:59 +0100

    initial Commit

---
+++ Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Ant\MetadataBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction($name)
+    {
+        return $this->render('MetadataBundle:Default:index.html.twig', array('name' => $name));
+    }
+}
---
+++ MetadataBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Ant\MetadataBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class MetadataBundle extends Bundle
+{
+}
